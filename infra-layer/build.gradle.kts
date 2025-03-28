524402bad9801a08e089ded3c35f0f71220f52ee
JeongHunHui <deca3248@gmail.com>
2025-03-28 18:05:38 +0900

    redis 관련 의존성, 설정, docker-compose 추가

--- infra-layer/build.gradle.kts
+++ infra-layer/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     // jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
     // mysql
     runtimeOnly("com.mysql:mysql-connector-j")
 
---
+++ infra-layer/src/main/kotlin/com/hunhui/ticketworld/infra/redis/RedisConfig.kt
@@ -0,0 +1,53 @@
+package com.hunhui.ticketworld.infra.redis
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}")
+    private val host: String,
+    @Value("\${spring.data.redis.port}")
+    private val port: Int,
+    @Value("\${spring.data.redis.password}")
+    private val password: String,
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = host
+        redisStandaloneConfiguration.port = port
+        redisStandaloneConfiguration.password = RedisPassword.of(password)
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val objectMapper =
+            ObjectMapper().apply {
+                registerModule(JavaTimeModule())
+                registerKotlinModule()
+                activateDefaultTyping(
+                    polymorphicTypeValidator,
+                    ObjectMapper.DefaultTyping.NON_FINAL,
+                )
+            }
+        val serializer = GenericJackson2JsonRedisSerializer(objectMapper)
+        return RedisTemplate<String, Any>().apply {
+            connectionFactory = redisConnectionFactory
+            keySerializer = StringRedisSerializer()
+            valueSerializer = serializer
+        }
+    }
+}
